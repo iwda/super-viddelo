c2953b069f8b6d460f3591753b47f660effbc436
iwda <david.iwertowski@gmx.de>
2018-02-05 10:52:31 +0100

    initial commit

---
+++ public/js/sprites.js
@@ -0,0 +1,11 @@
+import {loadImage} from './loaders.js';
+import SpriteSheet from './SpriteSheet.js';
+
+export function loadMarioSprite(){
+    return loadImage('/img/characters.gif')
+        .then(image => {
+            const sprites = new SpriteSheet(image, 16, 16);
+            sprites.define('idle', 276, 44, 16, 16);
+            return sprites
+        });
+}
