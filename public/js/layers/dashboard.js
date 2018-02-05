c4219f9263ab276bacb4ab53715db25f1ca78ae4
iwda <david.iwertowski@gmx.de>
2018-02-05 22:30:34 +0100

    new commit

--- public/js/layers/dashboard.js
+++ public/js/layers/dashboard.js
@@ -4,7 +4,7 @@ export function createDashboardLayer(font, playerEnv) {
     const LINE2 = font.size * 2;
 
     
-    const coins = 00;
+    const coins = 0;
 
     return function drawDashboard(context) {
         const {score, time} = playerEnv.playerController;
@@ -15,7 +15,7 @@ export function createDashboardLayer(font, playerEnv) {
         font.print('@x' + coins.toString().padStart(2, '0'), context, 96, LINE2);
 
         //font.print('WORLD', context, 152, LINE1);
-        font.print('DEVELOP', context, 152, LINE1);
+        font.print('DEVELOPment', context, 152, LINE1);
         //font.print('1-1', context, 160, LINE2);
         font.print('ALPHA', context, 160, LINE2);
 
